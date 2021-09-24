ea85edfd8644658c748339924afeabd705dac75b
Dong Nguyen <dongbuh@gmail.com>
2021-09-24 16:25:06 +0700

    Add new route

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ app.get('/', function (req, res) {
     res.json({ status: 200, mesage: 'Hello world' })
 });
 
+
+app.get('/about-me', function (req, res) {
+    res.json({ status: 200, mesage: 'Hello, my name is Dong Nguyen. Nice to meet you.' })
+});
+
 app.listen(4001, function () {
     console.log(`Server is running on port 4001`);
-})
+});
